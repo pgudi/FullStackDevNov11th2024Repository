100611e3f4a1ea8f09d9252a61cefea061d4a4af
pgudi <prabhakargudi@gmail.com>
2025-01-31 10:17:02 +0530

    Student Crud Operations

---
+++ springboot-student/src/main/java/com/gentech/student/dto/StudentDto.java
@@ -0,0 +1,49 @@
+package com.gentech.student.dto;
+
+public class StudentDto {
+    private Integer studentId;
+    private String firstName;
+    private String courseName;
+    private String emailId;
+
+    public StudentDto(){
+
+    }
+    public StudentDto(String firstName, String courseName, String emailId) {
+        this.firstName = firstName;
+        this.courseName = courseName;
+        this.emailId = emailId;
+    }
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Integer studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+}
