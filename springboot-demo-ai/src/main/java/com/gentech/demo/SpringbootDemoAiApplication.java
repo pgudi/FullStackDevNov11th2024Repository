172d8a18021b83b43fa1169e9b73c280e4440b7b
pgudi <prabhakar.gudi@gmail.com>
2025-03-28 08:48:26 +0530

    Spring AI Implementation

---
+++ springboot-demo-ai/src/main/java/com/gentech/demo/SpringbootDemoAiApplication.java
@@ -0,0 +1,13 @@
+package com.gentech.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoAiApplication.class, args);
+	}
+
+}
